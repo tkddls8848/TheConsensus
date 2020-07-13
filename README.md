266a0acb2d3efdd077b987ecf8e786fe3caecee7
tkddls8848 <tkddls8848@gmail.com>
2020-07-13 20:20:17 +0900

    Crwaling many pages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# TheConsensus" 
+# TheConsensus
+<br>
+"This Project is analyzing trader`s mind when cought into high volatility situation in BitMEX"
--- webCrawling.py
+++ webCrawling.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
 
-# 웹 드라이버 import
-# 페이지 html 코드 받기
+# 웹 드라이버
 driver = webdriver.Chrome("./chromedriver")
-driver.get("https://gall.dcinside.com/mgallery/board/lists?id=jusik")
-
+# 여러 페이지 받기
+for i in range(1,6):
+    driver.get("https://gall.dcinside.com/mgallery/board/lists/?id=jusik&page=" + str(i))
+    time.sleep(1) # 페이지 꼬임 방지
+    
 # 받은 코드 html로 변환
 soup = BeautifulSoup(driver.page_source, "html.parser")
-
+# 받은 문자 리스트
 result_list = []
 
 # html 코드 중 게시글의 제목만 받기
