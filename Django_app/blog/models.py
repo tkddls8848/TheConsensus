2a7bba07e8cd42e1bf6e74441728f9b3315f19e2
PSI <tkddls8848@gmail.com>
2020-07-25 18:53:03 +0900

    create blog App

---
+++ Django_app/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=True)
+
